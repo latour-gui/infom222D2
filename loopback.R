44130812d3068b8c728d80e4e22e303b17798a93
Tazoeur <g0latour@gmail.com>
2020-11-03 11:49:49 +0100

    change C -> R code style

--- loopback.R
+++ loopback.R
@@ -2,8 +2,8 @@
 # Created by: Guillaume <taz> Latour
 # Created on: 02/11/2020
 
-arrival <- function(m = 30, sd = 10) {
-  rnorm(1, m, sd)
+arrival <- function(n = 1, m = 30, sd = 10) {
+  rnorm(n, m, sd)
 }
 
 service <- function(m = 30, sd = 10) {
@@ -14,47 +14,52 @@ loopback <- function(p = 0.1) {
   runif(1) < p
 }
 
-ssqlb <- function(max_time, loopback_p = 0.1, arrival_mean = 30, arrival_sd = 10, service_mean = 30, service_sd = 10) {
-  tho <- max_time
-  arrival_count <- service_count <- l <- 0
-  a <- arrival(arrival_mean, arrival_sd)
-  c <- Inf
+partial_sum <- function(elems) {
+  tab <- numeric(length(elems))
+  for (i in seq_along(elems)) {
+    tab[i] <- sum(elems[1:i])
+  }
+  tab
+}
 
-  while ((a < tho) | (l > 0)) {
-    t <- min(a, c)
-    if (t == a) {
-      # add job to server
-      arrival_count <- arrival_count + 1
-      l <- l + 1
+ssqlb <- function(n, loopback_p = 0.1, arrival_mean = 30, arrival_sd = 10, service_mean = 30, service_sd = 10) {
+  arrivals <- arrival(n, arrival_mean, arrival_sd)
+  queue <- arrivals %>% partial_sum
+  services <- delays <- numeric(0)
+  loop <- busy <- 0
+  while (length(queue) > 0) {
+    # sort queue
+    queue <- queue[order(queue)]
 
-      # compute next job arrival
-      a <- a + arrival(arrival_mean, arrival_sd)
-      if (a > tho) {
-        a <- Inf
-      }
+    # pop first element
+    t <- queue[1]
+    queue <- queue[-1]
 
-      # if no queue, compute next job completion time
-      if (l == 1) {
-        c <- t + service(service_mean, service_sd)
-      }
+    # if server is busy
+    if (t < busy) {
+      # count the delay
+      delays <- c(delays, busy - t)
+      # fastforward to the time when the server is not busy anymore
+      t <- busy
     }
-    if (t == c) {
-      service_count <- service_count + 1
-      # job may be quitting server
-      if (!loopback(loopback_p)) {
-        l <- l - 1
-      }
 
-      # compute next job completion time
-      if (l > 0) {
-        c <- t + service(service_mean, service_sd)
-      } else {
-        c <- Inf
-      }
+    # now the service time of this job can be calculated
+    s <- service(service_mean, service_sd)
+    services <- c(services, s)
+    # so the server will be busy untill this time
+    busy <- t + s
+
+    # if there is a loopback, the job is queued right after quitting the server
+    if (loopback(loopback_p)) {
+      loop <- loop + 1
+      queue <- c(queue, busy)
     }
   }
 
-  data.frame(arrival_count, service_count)
+  print(sprintf('arrivals : %s', length(arrivals)))
+  print(sprintf('services : %s', length(services)))
+  print(sprintf('loops : %s', loop))
+  delays
 }
 
-ssqlb(5000)
+ssqlb(10)
